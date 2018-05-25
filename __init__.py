ffe92a4b41de181a2d9e64cef6009b6ed7959305
Jared Turpin <help.fool@versentiedge.com>
2018-05-25 07:54:07 -0600

    - included license information

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2018 Versentiedge LLC All Rights Reserved.
+Copyright (c) 2018 Versentiedge
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
