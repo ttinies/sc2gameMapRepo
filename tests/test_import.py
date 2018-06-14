46dcbf1ffebfaa4ac66d2fbeebf7ee01e8fcf5a9
Mike Henry <mhenry@handycodejob.com>
2018-06-13 20:54:34 -0600

    not sure why pytest isn't working

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,1 +1,6 @@
-import sc2maptool
+from sc2maptool import selectMap
+from sc2maptool.mapRecord import MapRecord
+
+def simple_test():
+    for m in selectMap(name="flat", melee=True, excludeName=True, closestMatch=False):
+        assert isinstance(m, MapRecord)
