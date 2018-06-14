e5865f63b8d04d6fa1118b0533de2c3767e881eb
Mike Henry <mhenry@handycodejob.com>
2018-06-13 21:28:36 -0600

    *slam head on desk* forgot the test_foo patterent for test discorvery

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,6 +1,6 @@
 from sc2maptool import selectMap
 from sc2maptool.mapRecord import MapRecord
 
-def simple_test():
+def test_simple():
     for m in selectMap(name="flat", melee=True, excludeName=True, closestMatch=False):
         assert isinstance(m, MapRecord)
