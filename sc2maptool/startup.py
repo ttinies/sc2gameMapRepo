0002668709afb70b4c9bc4d8cda8d1b3cffb6081
ttinies <ttinies.v@gmail.com>
2018-10-07 13:51:43 -0600

    - allow installation on machines without Starcraft 2 installed (i.e. travis cloud testing machines)

--- sc2maptool/startup.py
+++ sc2maptool/startup.py
@@ -13,8 +13,12 @@ from sc2maptool import constants as c
 def setup():
     """ensure that the maptool's maps are installed in the SC2 install maps """\
     """directory simply by importing this module"""
-    _cfg = config()
-    maptoolDir = os.path.join(_cfg.installedApp.mapsDir, c.PATH_MAPTOOLDIR)
+    try:
+        _cfg = config()
+        maptoolDir = os.path.join(_cfg.installedApp.mapsDir, c.PATH_MAPTOOLDIR)
+    except Exception as e: # allow installation on machines without Starcraft 2 installed (i.e. travis cloud testing machines)
+        print("WARNING: %s"%e)
+        maptoolDir = c.MAPS_FOLDER # default to the package's map library
     c.PATH_MAP_INSTALL = os.path.join(maptoolDir, __version__)
     if not os.path.isdir(c.PATH_MAP_INSTALL):
         if os.path.isdir(maptoolDir):  shutil.rmtree(maptoolDir) # clean up previous installations (avoid endlessly growing map repository)
