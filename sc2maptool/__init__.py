3d85271ab85f8118f44f3afc25b9f51ca171dcd7
ttinies <ttinies.v@gmail.com>
2018-10-07 13:14:52 -0600

    - renamed cli() as main()

--- sc2maptool/__init__.py
+++ sc2maptool/__init__.py
@@ -22,8 +22,8 @@ from __future__ import division       # python 2/3 compatibility
 from __future__ import print_function # python 2/3 compatibility
 
 # NOTE: Reference python sc2scenarios --help for usage descriptions
+from sc2maptool.__version__ import *
 from sc2maptool.functions import selectMap
 from sc2maptool.mapRecord import standardizeMapName
-from sc2maptool.cli import cli
+from sc2maptool.cli import main
 
-from sc2maptool.__version__ import *
